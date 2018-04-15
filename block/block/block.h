d09381b8b14c26b0c33da71c0e8005d782b05a2c
Benin Godfrey L <beningodfrey4@gmail.com>
2018-04-15 17:51:05 +0530

    added getters and setters in block

--- block/block/block.h
+++ block/block/block.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <vector>
 
-class block {
+class Block {
 private:
 	unsigned int height;
 	unsigned int timestamp;
@@ -13,7 +13,21 @@ private:
 	unsigned int nonce;
 	std::vector<Transaction> transactions;
 public:
-	block();
-	~block();
+	Block(unsigned int, std::string const&, std::vector<Transaction> const&);
+	unsigned int getHeight();
+	unsigned int getTimestamp();
+	std::string getPrevBlockHash();
+	std::string getBlockHash();
+	std::string getMerkleRoot();
+	Transaction getTransaction(unsigned int);
+	std::vector<Transaction> getTransaction();
+	void setHeight(unsigned int);
+	void setTimestamp();
+	void setPrevBlockHash(std::string const&);
+	void setBlockHash(std::string const&);
+	void setMerkleRoot(std::string const&);
+	void setNonce(unsigned int);
+	void setTransaction(Transaction const&, unsigned int);
+	void setTransaction(vector<Transaction> const&);
 };
 
